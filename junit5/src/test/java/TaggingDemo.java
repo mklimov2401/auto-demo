872649e7793a744098faf2ccb9ca2a8f6dc8e7fb
mklimov <mklimov2401@gmail.com>
2021-05-20 21:12:38 +0300

    Добавлен класс TaggingDemo.java

---
+++ junit5/src/test/java/TaggingDemo.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("fast")
+@Tag("model")
+class TaggingDemo {
+
+    @Test
+    @Tag("taxes")
+    void testingTaxCalculation() {
+    }
+
+}
